d3d88f91bf31b27a471d7fabfb61ab2de334f6ca
Marco <maguzm2@emory.edu>
2025-01-22 13:40:10 -0500

    Create file

---
+++ 566-ReshapetheMatrix/566-ReshapetheMatrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
+        length = len(mat) * len(mat[0])
+        if (r * c) > length or (r * c) < length: 
+            return mat
+
+        flatMatrix = []
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                flatMatrix.append(mat[i][j])
+
+        result = []
+        count = 0
+        for i in range(r):
+            row = []
+            for j in range(c):
+                row.append(flatMatrix[count])
+                count += 1
+            result.append(row)
+
+        return result
+                
