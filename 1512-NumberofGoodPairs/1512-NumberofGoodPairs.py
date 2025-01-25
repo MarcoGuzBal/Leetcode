4043d18dc400eae559f66bd4a8581efb434e5b12
Marco <maguzm2@emory.edu>
2025-01-25 10:49:51 -0500

    Update file 1/25/2025, 10:49:51 AM

--- 1512-NumberofGoodPairs/1512-NumberofGoodPairs.py
+++ 1512-NumberofGoodPairs/1512-NumberofGoodPairs.py
@@ -2,18 +2,24 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
 
         goodPair = 0
-        dictionary = defaultdict(list)
+        count = defaultdict(int)
 
-        for i in range(len(nums)):
-            dictionary[nums[i]].append(i)
+        for num in nums:
+            goodPair += count[num]  # Add the count of the current number to good pairs
+            count[num] += 1   
 
-        for i in range(len(nums)):
-            currNum = nums[i]
-            for j in (dictionary[currNum]):
-                if i == j:
-                    continue
-                elif i < j:
-                    goodPair += 1
+
+
+        # for i in range(len(nums)):
+        #     dictionary[nums[i]].append(i)
+
+        # for i in range(len(nums)):
+        #     currNum = nums[i]
+        #     for j in (dictionary[currNum]):
+        #         if i == j:
+        #             continue
+        #         elif i < j:
+        #             goodPair += 1
         
 
         return goodPair
