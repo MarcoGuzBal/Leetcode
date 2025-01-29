a240d52743dff549a22d18964930eb423db6dbf2
Marco <maguzm2@emory.edu>
2025-01-28 23:47:45 -0500

    Update file 1/28/2025, 11:47:45 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,19 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        n= max(len(word1), len(word2))
+        for i in range(n):
+            if i <len(word1):
+                result.append(word1[i])
+            if i<len(word2):
+                result.append(word2[i])
+        return "".join(result)
         
-        p1 = 0
-        p2 = 0
-
-        res = ""
-
-        while p1 < len(word1) or p2 < len(word2):
-
-            if p1 < len(word1):
-                res = res + word1[p1]
-                p1 += 1
-            if p2 < len(word2):
-                res = res + word2[p2]
-                p2 += 1
-
-        return res
-            
