56bf753fc19da59197d9042e3e26d63728af6069
Marco <maguzm2@emory.edu>
2025-02-01 11:19:46 -0500

    Update file 2/1/2025, 11:19:46 AM

--- 2068-CheckWhetherTwoStringsareAlmostEquivalent/2068-CheckWhetherTwoStringsareAlmostEquivalent.py
+++ 2068-CheckWhetherTwoStringsareAlmostEquivalent/2068-CheckWhetherTwoStringsareAlmostEquivalent.py
@@ -1,30 +1,15 @@
 class Solution:
     def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
         
-        map1 = {}
-        map2 = {}
+        map1 = Counter(word1)
+        map2 = Counter(word2)
         combined = word1 + word2
-        print(combined)
 
-        for char in word1:
-            if char in map1:
-                map1[char] += 1
-            else:
-                map1[char] = 1
-        
-        for char in word2:
-            if char in map2:
-                map2[char] += 1
-            else:
-                map2[char] = 1
+        # Instead use Counter
 
-        print(map1)
-        print(map2)
         for letter in combined:
             l1 = map1.get(letter, 0)
             l2 = map2.get(letter, 0)
-
-
 
             if abs(l1 - l2) > 3:
                 return False
