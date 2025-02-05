b0764ab4e05516aacbf8fba549609c1cf2989bb0
Marco <maguzm2@emory.edu>
2025-02-05 01:14:32 -0500

    Update file 2/5/2025, 1:14:32 AM

--- 682-BaseballGame/682-BaseballGame.py
+++ 682-BaseballGame/682-BaseballGame.py
@@ -1,22 +1,23 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        stack = []
-
-        for operation in operations:
-            print(stack)
-            if operation == '+':
-                num1 = stack[-1]
-                num2 = stack[-2]
-                stack.append(num1 + num2)
-            elif operation == 'D':
-                num1 = stack[-1]
-                stack.append(num1*2)
-            elif operation == 'C':
-                stack.pop()
+        stk = []
+        for i in operations:
+            if i =='C':
+                stk.pop()
+            elif i =='D':
+                d = stk[-1]
+                d = 2 * int(d)
+                stk.append(d)
+            elif i =="+":
+                temp1 = stk.pop()
+                temp2 = stk.pop()
+                temp3 = int(temp1) + int(temp2)
+                stk.append(temp2)
+                stk.append(temp1)
+                stk.append(temp3)
             else:
-                stack.append(int(operation))
-        
-        if len(stack) == 0:
-            return 0
-        else:
-            return sum(stack)
+                stk.append(i)
+        score = 0
+        while stk:
+            score += int(stk.pop())
+        return score
