6ea6e9603f8a7ed74772a37ab7e70a8e00e7ba2f
Marco <maguzm2@emory.edu>
2025-02-06 00:06:02 -0500

    Update file 2/6/2025, 12:06:02 AM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,13 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        hashmap = defaultdict(int)
-        count = len(nums) // 2
+        # hashmap = defaultdict(int)
+        # count = len(nums) // 2
+
+        # for i in nums:
+        #     hashmap[i] += 1
+        #     if hashmap[i] > (count):
+        #         return i
+
+        count = 0
+        candidate = 0
 
         for i in nums:
-            hashmap[i] += 1
-            if hashmap[i] > (count):
-                return i
+            if count == 0:
+                candidate = i
+                count += 1
+            elif i != candidate:
+                count -= 1
+            else:
+                count += 1
+
+        return candidate
+
                 
 
         
