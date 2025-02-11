e77806c1a1d9d719d1ee75338abaf78f68e4fa7a
Marco <maguzm2@emory.edu>
2025-02-11 00:15:43 -0500

    File updated at 2/11/2025, 12:15:43 AM

--- 2133-CheckifEveryRowandColumnContainsAllNumbers/2133-CheckifEveryRowandColumnContainsAllNumbers.py
+++ 2133-CheckifEveryRowandColumnContainsAllNumbers/2133-CheckifEveryRowandColumnContainsAllNumbers.py
@@ -1,26 +1,17 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-
+        
         n = len(matrix)
+        expected_set = set(range(1, n + 1))
 
-        for i in range(n):
-            rowSet = set()
-            for num in matrix[i]:
-                rowSet.add(num)
-            for j in range(1, n + 1):
-                if j not in rowSet:
-                    return False
+        # Check rows
+        for row in matrix:
+            if set(row) != expected_set:
+                return False
 
-        for i in range(n):
-            colSet = set()
-            for j in range(n):
-                currNum = matrix[j][i]
-                colSet.add(currNum)
-            for k in range(1, n + 1):
-                if k not in colSet:
-                    return False
+        # Check columns
+        for col in zip(*matrix):
+            if set(col) != expected_set:
+                return False
 
         return True
-
-        
-        
