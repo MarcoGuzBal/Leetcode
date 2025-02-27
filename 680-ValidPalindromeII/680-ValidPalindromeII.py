75221afc5bb624ed8f8ff90572c815a4c36fd608
Marco <maguzm2@emory.edu>
2025-02-26 19:49:25 -0500

    File updated at 2/26/2025, 7:49:25 PM

--- 680-ValidPalindromeII/680-ValidPalindromeII.py
+++ 680-ValidPalindromeII/680-ValidPalindromeII.py
@@ -1,10 +1,9 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-
+        '''
         def is_palindrome(sub_s):
             return sub_s == sub_s[::-1]
 
-        delete = True
         l = 0
         r = len(s) - 1
 
@@ -15,4 +14,21 @@ class Solution:
             else:
                 return is_palindrome(s[l+1:r+1]) or is_palindrome(s[l:r])
             
+        return True
+        '''
+        left = 0 
+        right = len(s) - 1
+    
+        while left < right:
+            if s[left] != s[right]:
+            
+                leftCurr = s[left+1:right+1]
+                rightCurr = s[left:right]
+            
+                return leftCurr == leftCurr[::-1] or rightCurr == rightCurr[::-1]
+        
+            else:
+                left += 1
+                right -= 1
+
         return True
