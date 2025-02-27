838cb7b60fa5c097c0ff73dc5ec63d16d080644b
Marco <maguzm2@emory.edu>
2025-02-27 00:57:17 -0500

    File updated at 2/27/2025, 12:57:17 AM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.py
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.py
@@ -1,11 +1,13 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        
-        result = [True] * len(candies)
 
-        for i in range(len(candies)):
-            if candies[i] + extraCandies < max(candies):
-                result[i] = False
+        max_num=max(candies)
 
-        return result
-                
+        res=[]
+
+        for i in candies:
+            if i+extraCandies >= max_num:
+                res.append(True)
+            else:
+                res.append(False)
+        return res
