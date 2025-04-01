62bfb2c73f2821ea805afdf6f977c955f147d301
Marco <maguzm2@emory.edu>
2025-04-01 10:24:08 -0400

    Create file

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,13 @@
+// Last updated: 4/1/2025, 10:24:08 AM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
