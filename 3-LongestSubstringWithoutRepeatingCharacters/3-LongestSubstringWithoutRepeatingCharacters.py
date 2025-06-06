b4939ad650f32f5b6beaae4e1391da54ffe9cf18
Marco <maguzm2@emory.edu>
2025-06-06 16:59:59 -0400

    File updated at 6/6/2025, 4:59:59 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,40 +1,14 @@
-# Last updated: 6/6/2025, 4:59:27 PM
+# Last updated: 6/6/2025, 4:59:59 PM
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        l, res = 0, 0
 
-        l = 0
-        r = 0
-        seen = set() 
-        max_len = 0
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(l, mp[s[r]] + 1)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
 
-        while r < len(s):
-            if s[r] in seen:
-                max_len = max(len(seen), max_len)
-                while s[r] in seen:
-                    seen.remove(s[l])
-                    l += 1
-            else:
-                seen.add(s[r])
-                r += 1
-
-        max_len = max(len(seen), max_len)
-        return max_len
-
-        # left = 0
-        # right = 0
-        # maxLength = 0
-        # currString = ""
         
-        # while left < len(s) and right < len(s):
-        #     char = s[right]
-        #     if char in currString:
-        #         maxLength = max(maxLength, len(currString))
-        #         left += 1
-        #         right = left
-        #         currString = "" + s[right-1]
-        #     else:
-        #         currString += char
-        #         right += 1
-
-        # maxLength = max(maxLength, len(currString))
-        # return maxLength
