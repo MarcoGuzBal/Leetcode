b9cc95abcce5cbc7f8bf3e7f0a807d67e5a00fea
Marco <maguzm2@emory.edu>
2025-06-06 13:07:19 -0400

    File updated at 6/6/2025, 1:07:19 PM

--- 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.py
+++ 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.py
@@ -1,4 +1,4 @@
-# Last updated: 6/2/2025, 12:54:50 PM
+# Last updated: 6/6/2025, 1:07:19 PM
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         
@@ -7,18 +7,12 @@ class Solution:
         for i in range(1, n + 1):
             queue.append(i)
 
-        print(queue)
-
         while len(queue) != 1:
             pops = k
 
-            while pops != 0:
-                pops -= 1
+            for i in range(k - 1):
+                queue.append(queue.popleft())
 
-                if pops == 0:
-                    queue.popleft()
-                else:
-                    popped = queue.popleft()
-                    queue.append(popped)
+            queue.popleft()
 
         return queue.popleft()
