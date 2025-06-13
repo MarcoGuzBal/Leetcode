d4ef9fdf88c322bdbd8f36302afd6ed8726ee7db
Marco <maguzm2@emory.edu>
2025-06-13 16:01:15 -0400

    File updated at 6/13/2025, 4:01:15 PM

--- 637-AverageofLevelsinBinaryTree/637-AverageofLevelsinBinaryTree.py
+++ 637-AverageofLevelsinBinaryTree/637-AverageofLevelsinBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 6/13/2025, 4:00:23 PM
+# Last updated: 6/13/2025, 4:01:15 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,6 +7,23 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        # answer = []
+        # q = deque()
+        # q.append(root)
+
+        # while q:
+        #     length = len(q)
+        #     mySum = 0
+        #     for _ in range(length):
+        #         popped = q.popleft()
+        #         mySum += popped.val
+        #         if popped.left:
+        #             q.append(popped.left)
+        #         if popped.right:
+        #             q.append(popped.right)
+        #     answer.append(mySum / length)
+        # return answer
+
         queue = deque([root])
         result = []
 
@@ -22,4 +39,3 @@ class Solution:
             result.append(sum(level)/len(level))
 
         return result
-                
