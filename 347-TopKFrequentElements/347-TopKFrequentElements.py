2e1d1b751372ffd195d542876716cc3bf997cfc5
Marco <maguzm2@emory.edu>
2025-06-24 17:03:55 -0400

    File updated at 6/24/2025, 5:03:55 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,71 +1,74 @@
-# Last updated: 6/24/2025, 5:03:31 PM
+# Last updated: 6/24/2025, 5:03:55 PM
 class Solution:
+    def special_merge(self, iinput):
+        if len(iinput) == 1: return iinput
+        elif len(iinput) == 2:
+            if iinput[1][1] < iinput[0][1] :  return iinput
+            else: return iinput[::-1]
+        else:
+            mid = len(iinput)//2
+            k1 = self.special_merge(iinput[:mid]);print (k1)
+            k2 = self.special_merge(iinput[mid:]);print (k1)
+
+            result = []
+
+            while True:
+                if k1[0][1] > k2[0][1] :
+                    result.append(k1[0]); k1.pop(0)
+                else:
+                    result.append(k2[0]); k2.pop(0)
+
+                if not k1: return result + k2
+                if not k2: return result + k1
+
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Brute Force
-        # Time Complexity: O(N^2)
-        # Space Complexity: O()
-        # counter = {}
-        # result = []
 
-        # for num in nums:
-        #     if num in counter:
-        #         counter[num] += 1
-        #     else:
-        #         counter[num] = 1
+        d1 = {}
+        for i in nums:
+            d1[i] = d1.get(i,0) + 1
+        print (" Unsorted dictionary based on values look like this {}".format(d1))
 
-        # buckets = [[] for _ in range(len(nums) + 1)]
+        rr = self.special_merge([(k,v) for k,v in d1.items()])
+        print(" Sorted dictionary based on values look like this {}".format(rr))
+        return [t[0] for t in rr[:k]]    
+    def topKFrequent_2(self, nums: List[int], k: int) -> List[int]:
 
-        # for key in counter:
-        #     buckets[counter[key]].append(key)
+        d1 = {}
+        for i in nums:
+            d1[i] = d1.get(i,0) + 1
+
+        d_values = list(d1.values())
+        d_keys = list(d1.keys())
+
+        while True:
+            status = False
+            for j in range(len(d_values)-1):
+                if d_values[j] < d_values[j+1]:
+                    d_values[j] , d_values[j + 1] =  d_values[j+1], d_values[j]
+                    status = True
+                    d_keys[j], d_keys[j + 1] = d_keys[j + 1], d_keys[j]
+            if status==False: 
+                break
+        return d_keys[:k]
 
 
-        # for i in range(len(buckets)-1, -1, -1):
-        #     for num in buckets[i]:
-        #         result.append(num)
-        #         k -= 1
-        #         if k == 0:
-        #             return result
-            
-        # return result
-
-        hashmap = Counter(nums)
-        result = []
-        buckets = [[] for _ in range(len(nums) + 1)]
+    def topKFrequent_2(self, nums, k):
+        """
+        Approach 1: Using Min Heap
+        Time Complexity: O(n log k)
+        Space Complexity: O(n + k)
+        """
+        # Count frequencies
+        count = Counter(nums)
         
-        for key in hashmap:
-            buckets[hashmap[key]].append(key)
-
-        for i in range(len(buckets)-1, -1, -1):
-            bucket = buckets[i]
-            print(bucket)
-            if k == 0:
-                return result
-            
-            if bucket:
-                for num in buckets[i]:
-                    if k == 0:
-                        return result
-                    result.append(num)
-                    k -= 1
-                    
+        # Use min heap to keep track of top k elements
+        heap = []
         
+        for num, freq in count.items():
+            heapq.heappush(heap, (freq, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # Extract elements from heap
+        return [num for freq, num in heap]        
