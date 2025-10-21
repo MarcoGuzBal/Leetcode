f9eadf0a1767c1f721b29e2186bc89f78a65335c
Marco <maguzm2@emory.edu>
2025-10-20 20:40:23 -0400

    File updated at 10/20/2025, 8:40:23 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,11 +1,22 @@
+# Last updated: 10/20/2025, 8:40:23 PM
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        hashmap = defaultdict(list)
+        # Time Complexity: (K N log N)
+        # Space Complexity: (NK) since at most N unique Keys and K unique values
 
-        for string in strs:
-            sorted_string = "".join(sorted(string))
-            hashmap[sorted_string].append(string)
+        hashmap = {}
+        result = []
 
-        print(hashmap.values())
-        return list(hashmap.values())
+        for word in strs:
+            sortedWord = "".join(sorted(word))
+
+            if sortedWord in hashmap:
+                hashmap[sortedWord].append(word)
+            else:
+                hashmap[sortedWord] = [word]
+
+        for key in hashmap:
+            result.append(hashmap[key])
+
+        return result
